d68294d299a4a55818d2c9ccd79bcb5e7337361f
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 20:52:00 +0100

    commit zero

---
+++ Mad City Script pastebin roblox Mad City Super GUI .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://system-exodus.com/scripts/madlads/MadLads.lua",true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Very good script for Mad City. There are a lot of functions that are needed for users of this place. What is in this Gui: Auto-farm, Give Emotions and More
+						
+]]
